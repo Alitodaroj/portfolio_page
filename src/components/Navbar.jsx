651948da5ddf50f706b80ed959cdc9b316b62499
Alitodaroj <aroj34@yahoo.com>
2023-11-06 10:32:16 -0500

    bootstrap added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,30 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { Navbar } from 'react-bootstrap';
 
+function selectAll(selector) {
+    return document.querySelectorAll(selector);
+  }
+  
+  let navbarLinks = Array.from(selectAll('#navbar .scrollto'));
+  
+  const navbarLinksActive = () => {
+    let position = window.scrollY + 200;
+    navbarLinks.forEach(navbarLink => {
+      if (!navbarLink.hash) return;
+      let section = selectAll(navbarLink.hash);
+      if (!section) return;
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarLink.classList.add('active');
+      } else {
+        navbarLink.classList.remove('active');
+      }
+    });
+  };
+  
+  window.addEventListener('load', navbarLinksActive);
+  window.addEventListener('scroll', navbarLinksActive);
+  
+
 const NavBar = () => {
     return (
         <Navbar expand="dark" className="bg-body-tertiary fixed-top" variant="light">
