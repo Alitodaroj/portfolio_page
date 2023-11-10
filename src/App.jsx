a13ee852a21f3a5227186718246fc8c707e5c811
Alitodaroj <aroj34@yahoo.com>
2023-11-10 17:16:05 -0500

    aside added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,23 @@ import { BrowserRouter } from "react-router-dom";
 import { MyRoutes }  from "./components/Routes";
 import Navbar from "./components/Navbar";
 import "./App.css";
+import { Link } from "react-router-dom";
+import { Nav } from "react-bootstrap";
+
+function Aside() {
+  return <>
+    <Nav.Link as={Link} to="/">Home</Nav.Link>
+    <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
+        </>  
+}
 
 const App = () => {
   return (
       <BrowserRouter>
         <Navbar />
+        <Aside>
+         { Aside }
+        </Aside>
         <MyRoutes />
       </BrowserRouter>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { Navbar } from 'react-bootstrap';
-import { Link } from "react-router-dom";
 
 function NavBar() {
     const [expanded, setExpanded] = useState(false);
@@ -26,8 +25,6 @@ function NavBar() {
                 <Nav.Link onClick={() => scrollToSection('about')}>About Me</Nav.Link>
                 <Nav.Link onClick={() => scrollToSection('skills')}>Skills</Nav.Link>
                 <Nav.Link onClick={() => scrollToSection('contact')}>Contact Me</Nav.Link>
-                <Nav.Link as={Link} to="/">Home</Nav.Link>
-                <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
             </Nav>
             </Navbar.Collapse>
             </Container>
