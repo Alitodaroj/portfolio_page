393037af3ae3ee37605bacb778bb06109e0adda7
Alitodaroj <aroj34@yahoo.com>
2023-11-13 12:21:22 -0500

    comments section edit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,23 +1,60 @@
-import React from "react";
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-import { Button } from "react-bootstrap";
+import Row from 'react-bootstrap/Row';
 
-export default function Contact() {
-    return(
-        <section id="contact" classname="mt-4 mb-5">
-             <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Enter Text Here</Form.Label>
-        <Form.Control as="textarea" rows={4} />
-      </Form.Group>
+function FormExample() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>First name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="First name"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Last name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Last name"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+       
+            
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="6" controlId="validationCustom03">
+          <Form.Label>Comments</Form.Label>
+          <Form.Control type="text" placeholder="Comments" required />
+          <Form.Control.Feedback type="invalid">
+            Comments
+          </Form.Control.Feedback>
+        </Form.Group>
+        
+      </Row>
+     
+      <Button type="submit">Submit form</Button>
     </Form>
-    <Button variant ="primary" type="submit">
-        Submit
-    </Button>
-    </section>
-    )
+  );
 }
+
+export default FormExample;
